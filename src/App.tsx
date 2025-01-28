cf716b2f67f0b1ad9879b54fcd4b873a96ae3485
Aman Tripathi <aman.tripathi@shipglobal.in>
2025-01-28 15:36:45 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <h1>hello word</h1>
+      <h1 className="text-5xl">hello word</h1>
        <Button>hello</Button>
     </>
   );
